f391dfe350058fc0b791297413c711ecb216c2a8
callas4ever <thetseringnorbu@gmail.com>
2020-06-09 13:34:44 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,18 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+  def new 
+    @post=Post.new
+  end
+
+  def create 
+    post=Post.create(post_params)
+    
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,19 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  def new
+    @user=User.new
+  end
+  def create
+    @user=User.create(user_params)
+    redirect_to user_path(@user)
+  end
+
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email)
+  end
 end
